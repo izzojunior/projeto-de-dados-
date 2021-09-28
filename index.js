c49c21d81c592d5747f6a620daddb00366ddfb37
IzzoJunior <flaviojdjunior@gmail.com>
2021-09-28 20:43:28 -0300

    atualizand

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-// 1) quantas rodada
-// 2) quantos jogadors
-// 3) objeto pra cada jogador
-// 4) guardar os objetos numa lista
-// 5) ordenar pelo numero no dado
-// 6) exibir quem ganhou mais rodadas
-
 const prompt = require('prompt-sync')()
 
 function getMostFrequent(arr) {
@@ -59,63 +52,3 @@ for(let i = 0; i < rodadasQtd ; i++){
 const grandeVencedor = getMostFrequent(vencedores);
 
 console.log(`O grande vencedor foi o jogador ${grandeVencedor}, ${jogadores[grandeVencedor].nome}!`);
-
-
-
-
-
-// if (jogos[0].numero > jogos[1].numero) {
-//     console.log(`-----------------------------------`)
-//     console.log(`${jogos[0].nome} vence a rodada!!`);
-//     console.log(`-----------------------------------`)
-//     vencedor = jogos[0].nome
-// }
-// else if (jogos[0].numero == jogos[1].numero) {
-//     console.log(`----------------------------
-// // Collections.sort(pontuacao);
-// Collections.reverse(pontuacao);
-
-// console.log(sort);
-// for (i = 0; i < qntJogadores; i++) {
-// lista.sort((a, b) => b.numero - a.numero)
-// Collections.sort(pontuacao);
-// Collections.reverse(pontuacao);
-// console.log(sort);
-
-// const sorted = Jogadores.sort((a,b) => {
-//     return a.pontuacao - b.pontuacao;
-// console.log(sorted);
-// }
-
-
-
-
-// // 5 5 5 = 15
-// // 6 6 1 = 13
-// // 6 1 1 = 8
-
-//  6 6 1 = 13
-//  5 5 5 = 15
-//  6 1 1 = 8
-
-//  Qual o parametro de ordenação?
-//   - Soma do total dos dados?
-//  - Nome do jogador?
-
-//  Qual o criterio pro vencedor?
-//   - Vitória por rodada?
-//   - Vitória na soma total de dados?
- 
-//  jogadoresQtd = 3
- 
-// jogadores = [
-//     { name: 'junior', pontuacao: [] }
-//     { name: 'alexandre', pontuacao: [] }
-//     { name: 'juliana', pontuacao: [] }
-
-// jogadores[0] === { name: 'junior', pontuacao: [] }
-// jogadores[0].name === 'junior'
-// jogadores[0].pontuacao === [} 
-// jogadores[1] === { name: 'alexandre', pontuacao: [] }
-// jogadores[1].name === 'alexandre'
-// jogadores[1].pontuacao === []
